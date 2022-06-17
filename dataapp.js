62e5438a135b1c27fc8abd0194e8dca56c8253b3
Searchcept99 <faicalbahsis666@gmail.com>
2022-06-17 10:57:45 +0100

    test

---
+++ dataapp.js
@@ -0,0 +1,15 @@
+let users = [
+    {
+      id: "123456789",
+      createdDate: "2021-01-06",
+      status: "En validation",
+      firstName: "Souhaib",
+      lastName: "halli",
+      userName: "halli",
+      registrationNumber: "2584",
+    },
+    
+  ]
+  
+  
+  export default users;
